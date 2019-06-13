1c7c59a0b3c241f0a531a313aae102292b1ea30c
Ernesto Berrios <eberrios@aceroschile.cl>
2019-06-13 14:17:57 -0400

    refactored index page to use Post partial

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+
+
  describe "Creation" do
+
   before do
-      #@post = Post.create(date: Date.today, rationale: "Anything", user_id: current_user.id)
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Ernesto", last_name: "Berrios")
+      login_as(@user, :scope => :user)
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id:  @user.id)
   end
     it "can be created" do
-      #expect(@post).to be_valid
+      expect(@post).to be_valid
     end
 
     it "cannot be created without a date and rationale" do
-      #@post.date = nil
-      #@post.rationale = nil
-      #@post.user_id = nil
-      #expect(@post).to_not be_valid
+      @post.date = nil
+      @post.rationale = nil
+      @post.user_id = nil
+      expect(@post).to_not be_valid
     end 
   end
 end
