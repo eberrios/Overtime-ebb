a2f71a6a52c66455e5a5296acead5585e560eb00
Ernesto Berrios <eberrios@aceroschile.cl>
2019-06-13 13:28:27 -0400

    Implement index ciew with posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "test@test.com", password: "123456789", password_confirmation: "123456789", first_name: "The", last_name: "Boss")
+puts "1 user created"
+
+
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
