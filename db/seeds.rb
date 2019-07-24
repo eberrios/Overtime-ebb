f171422ac5cde1957ec38f9fe97235856260feb4
Ernesto Berrios <eberrios@aceroschile.cl>
2019-07-24 14:15:27 -0400

    Implemented audit log tab that is shown only to admin users

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,6 @@ puts '100 Posts have been created'
 100.times do |audit_log|
   AuditLog.create!(user_id: @user.id,
                    status: 0,
-                   start_date: (Date.today - 6.days)
-                  )
+                   start_date: (Date.today - 6.days))
 end
 puts '100 AuditLog have been created'
